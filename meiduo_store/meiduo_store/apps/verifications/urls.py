ed3d40dd0c4226805a974e45214d81e20ba28da9
yx3445 <18234452010@163.com>
2021-03-14 19:15:45 +0800

    配置短信功能路由

---
+++ meiduo_store/meiduo_store/apps/verifications/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'smscode/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),
+
+]
--- meiduo_store/meiduo_store/settings/dev.py
+++ meiduo_store/meiduo_store/settings/dev.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -180,3 +182,12 @@ LOGGING = {
         },
     }
 }
+
+# DRF配置项
+REST_FRAMEWORK = {
+    # 异常处理
+    'EXCEPTION_HANDLER': 'meiduo_store.utils.exceptions.exception_handler',
+}
+
+# 修改Django认证系统的用户模型类
+AUTH_USER_MODEL = 'users.User'
